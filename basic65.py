924de623925e702a2da7c9e955027d49bf86a292
SinghIsCoding <theriyasingh3@gmail.com>
2021-08-19 22:30:39 +0530

    .

--- basic65.py
+++ basic65.py
@@ -1,1 +1,9 @@
 # wap to input a number, if it is not a number generates and error message
+
+while True:
+    try:
+        a = int(input("input a number: "))
+        break
+    except ValueError:
+        print("\n This is not a number. Try again...")
+        print()
---
+++ basic66.py
@@ -0,0 +1,10 @@
+# Write a Python program to get the Python version you are using.
+
+# A string containing the version number of the Python interpreter plus additional information on the build number and compiler used. This string is displayed when the interactive interpreter is started.
+
+
+import sys
+print("Python version")
+print (sys.version)
+print("Version info.")
+print (sys.version_info)
